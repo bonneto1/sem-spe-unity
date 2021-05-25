6a1446248231c4abb0bffa536a6937381001b7e3
bonneto1 <bonneto@iut-acy.univ-smb.fr>
2021-05-25 15:34:58 +0200

    bateau fini

--- Assets/Bateau/Bateau_collision.cs
+++ Assets/Bateau/Bateau_collision.cs
@@ -4,14 +4,19 @@ using UnityEngine;
 
 public class Bateau_collision : MonoBehaviour
 {
-    private int NbCollisions;
+    private int nbCollisions;
+    private float intervalTire;
+    private float derniereTouche;
+    
     [SerializeField]
-    private int NbVies;
+    private int nbVies;
 
     // Start is called before the first frame update
     void Start()
     {
-        NbCollisions = 0;
+        nbCollisions = 0;
+        intervalTire = 3;
+        derniereTouche = 0;
     }
 
     // Update is called once per frame
@@ -22,11 +27,17 @@ public class Bateau_collision : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        
-        NbCollisions += 1;
-        if (NbCollisions >= 5)
+        if (intervalTire <= Time.realtimeSinceStartup - derniereTouche && collision.gameObject.name != "Plane")
         {
-            Destroy(this.gameObject);  
+          nbCollisions += 1;
+          Debug.Log("toucher");
+          if (nbCollisions >= nbVies)
+          {
+            Destroy(gameObject);
+            
+            
+          }
+          derniereTouche = Time.realtimeSinceStartup; 
         }
     }
 }
