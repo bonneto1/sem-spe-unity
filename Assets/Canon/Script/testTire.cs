fba82b0dda46307e3fda7d42dc4e9bc2d8b29a04
mortierq <quentin.mortier@etu.univ-smb.fr>
2021-05-25 13:40:24 +0200

    Ajout du prefab Canon
    ajout du script pour tirer et des prefab canon et boulet

---
+++ Assets/Canon/Script/testTire.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testTire : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    private int vitesse;
+    [SerializeField]
+    private float intervalTire;
+    [SerializeField]
+    private Rigidbody Boulet;
+
+    private float dernierTire;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(intervalTire <= Time.realtimeSinceStartup - dernierTire)
+        {
+            Rigidbody b = Instantiate(Boulet, transform.position, transform.rotation);
+            b.velocity = transform.forward * vitesse;
+            //Boulet.SetActive(true);
+
+
+            dernierTire = Time.realtimeSinceStartup;
+        }
+    }
+}
