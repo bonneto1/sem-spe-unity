d85f66f9825360e3211fe8ccb9434999712acd5e
mortierq <quentin.mortier@etu.univ-smb.fr>
2021-05-25 14:53:25 +0200

    maj collision boulet

--- Assets/Canon/Script/collision_Boulet.cs
+++ Assets/Canon/Script/collision_Boulet.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class collision : MonoBehaviour
+public class collision_Boulet : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -18,7 +18,7 @@ public class collision : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        if(collision.collider.name == "WaterProDaytime")
+        if (collision.collider.name == "WaterProDaytime" || collision.collider.name == "Plane")
         {
             Debug.Log("Plouf");
             Destroy(gameObject);
