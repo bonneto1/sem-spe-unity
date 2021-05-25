089160ef631a772a471bc0ddbee44c41f583a21a
bonneto1 <bonneto@iut-acy.univ-smb.fr>
2021-05-25 14:31:12 +0200

    code collisions bateau

---
+++ Assets/Bateau/Bateau_collision.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bateau_collision : MonoBehaviour
+{
+    private int NbCollisions;
+    [SerializeField]
+    private int NbVies;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        NbCollisions = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+        NbCollisions += 1;
+        if (NbCollisions >= 5)
+        {
+            Destroy(this.gameObject);  
+        }
+    }
+}
