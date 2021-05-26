e62fbad6b8cba6e5f891b5a043038be020cdc1d4
bonneto1 <bonneto@iut-acy.univ-smb.fr>
2021-05-26 14:34:21 +0200

    correction bug
    boutons menu fonctionnel

--- Assets/Scenes/script/MenuPrincipal.cs
+++ Assets/Scenes/script/MenuPrincipal.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MenuPrincipale : MonoBehaviour
+public class MenuPrincipal : MonoBehaviour
 {
     
     public void BoutonJouer()
