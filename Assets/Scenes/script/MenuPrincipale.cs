26dcf5435c467c86cba21e441f506d37fb5345e2
bonneto1 <bonneto@iut-acy.univ-smb.fr>
2021-05-26 10:29:08 +0200

    menu principale
    menu principale fini

---
+++ Assets/Scenes/script/MenuPrincipale.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPrincipale : MonoBehaviour
+{
+    
+    public void BoutonJouer()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void BoutonQuitter()
+    {
+        Debug.Log("Pouf");
+        Application.Quit();
+    }
+}
