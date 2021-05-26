618cf689208e8ec9de09cb40914777dece52893f
bonneto1 <bonneto@iut-acy.univ-smb.fr>
2021-05-26 16:27:25 +0200

    son menu,victoire,defaite
    les sons des menus, de victoire et de defaite qui ne marchent pas

--- Assets/Scenes/script/MenuPrincipal.cs
+++ Assets/Scenes/script/MenuPrincipal.cs
@@ -3,16 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class MenuPrincipal : MonoBehaviour
 {
-    
+    [SerializeField]
+    AudioSource m_source;
+
+    public void Start()
+    {
+        
+    }
     public void BoutonJouer()
     {
+
+        
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void BoutonQuitter()
     {
+        
+        
         Application.Quit();
     }
+    
 }
