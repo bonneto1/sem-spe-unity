c9ae48bc2729366abb0b9aeeae09f60205834d1b
bonneto1 <bonneto@iut-acy.univ-smb.fr>
2021-05-27 19:16:34 +0200

    menus
    menus fonctionnels

--- Assets/Scenes/script/ChangementMenu.cs
+++ Assets/Scenes/script/ChangementMenu.cs
@@ -1,27 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
-public class DetecteFinSon : MonoBehaviour
+public class ChangementMenu : MonoBehaviour
 {
     private bool m_lance;
     // Start is called before the first frame update
     void Start()
     {
         m_lance = false;
+        gameObject.GetComponent<AudioSource>().playOnAwake = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(GetComponent<AudioSource>().isPlaying)
+        if (GetComponent<AudioSource>().isPlaying)
         {
             m_lance = true;
         }
-        if(m_lance && !GetComponent<AudioSource>().isPlaying)
+        if (m_lance && !GetComponent<AudioSource>().isPlaying)
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            
         }
     }
 }
