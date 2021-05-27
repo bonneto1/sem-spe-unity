fa0918205fa9fa6f2c0906e5986956551e5f4deb
bonneto1 <bonneto@iut-acy.univ-smb.fr>
2021-05-27 10:45:07 +0200

    sons menu
    tous les sons  des menus marchent

---
+++ Assets/Scenes/script/QuitterJeu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitterJeu : MonoBehaviour
+{
+    private bool m_lance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_lance = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GetComponent<AudioSource>().isPlaying)
+        {
+            m_lance = true;
+        }
+        if (m_lance && !GetComponent<AudioSource>().isPlaying)
+        {
+            Application.Quit();
+        }
+    }
+}
